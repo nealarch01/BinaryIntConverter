a962706842efa892097228bf3b729ccae8db9186
Neal Archival <archivalneal@gmail>
2021-10-16 01:08:12 -0700

    added signed integer function and 64 bit limit

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ inputBox.addEventListener('keydown', (event)=> {
 })
 
 function validateInput() {
-    const strInput = String(document.getElementById("inputbox").value);
+    var strInput = String(document.getElementById("inputbox").value);
     for(let i = 0; i < strInput.length; i++) {
         if(strInput[i] == ' ') continue;
         const inputCh = strInput.charCodeAt(i);
@@ -39,24 +39,19 @@ function invalidInput(invChar) {
     document.getElementById("results").innerHTML = errorMsg;
 }
 
-function charToInt(someChar) {
-    let nInt = someChar.charCodeAt(0);
-    return nInt - 48;
-}
-
-// Unsigned conversion
 function convertBinToDec(input) {
     if(document.getElementById("signed").checked == true) {
-        signedBinToDec(input);
-    } else {
-        unsignedBinToDec(input);
+        if(input[0] === '1') {
+            signedBinToDec(input);
+            return;
+        }
     }
+    unsignedBinToDec(input);
 }
 
 function unsignedBinToDec(input) {
     var decResult = 0;
     var exponent = 0;
-    input = String(input);
     for(let i = input.length - 1; i >= 0; i--) {
         let singleBinaryValue = 0;
         switch(input[i]) {
@@ -65,6 +60,7 @@ function unsignedBinToDec(input) {
             case('1'):
                 singleBinaryValue = 2 ** exponent;
                 decResult += singleBinaryValue;
+                // no break needed, we want to increment exponent
             case('0'):
                 exponent++;
                 break;
@@ -73,14 +69,90 @@ function unsignedBinToDec(input) {
                 return;
         }
     }
-    var resultMsg = "Decimal: " + decResult.toString();
-    document.getElementById("results").innerHTML = resultMsg;
+    displayResult(decResult);
 }
 
 function signedBinToDec(input) {
-    
+    // first reverse all bits
+    var reversedInput = ""; // initialize as empty string
+    for(let i = 0; i < input.length; i++) {
+        switch(input[i]) {
+            case('0'):
+                reversedInput += '1';
+                break;
+            case('1'):
+                reversedInput += '0';
+                break;
+            case(' '): break; // do nothing
+            default:
+                invalidInput(input[i]);
+                return;
+        }
+    }
+    // once bits have been inverted, add one binary bit
+    var carry = '1';
+    var addedInput = ""; 
+    for(let i = reversedInput.length - 1; i >= 0; i--) {
+        if(carry === '1') {
+            if(reversedInput[i] === '1') {
+                addedInput += '0';
+            } else if(reversedInput[i] === '0') {
+                addedInput += '1';
+                carry = '0'; // no more to carry since we are adding with 0
+            } else { // if space
+                continue; 
+            }
+        } else {
+            addedInput += reversedInput[i];
+        }
+    }
+    reversedInput = ""; // reset
+    for(let i = addedInput.length - 1; i >= 0; i--) {
+        reversedInput += addedInput[i];
+    }
+    var exponent = 0;
+    var decResult = 0;
+    // no need to invalidate input since string has been 
+    for(let i = reversedInput.length; i >= 0; i--) {
+        let singleBinaryInput = 0;
+        switch(reversedInput[i]) {
+            case('1'):
+                singleBinaryInput = 2 ** exponent;
+                decResult += singleBinaryInput;
+            case('0'):
+                exponent++;
+                break;
+            case(' '):
+                break;
+        }
+    }
+    var finalRes = "-" + decResult.toString();
+    displayResult(finalRes);
 }
 
+function reverseStr(someBinaryString) {
+    var mid = floor(someBinaryString / 2);
+    var j = someBinaryString.length - 1;
+    for(let i = 0; i <= mid; i++) {
+        
+    }
+}
+
+function displayResult(finalResult) {
+    var resultMsg;
+    switch(operationType) {
+        case converter.BINTODEC:
+            resultMsg = "Decimal: ";
+            resultMsg += finalResult.toString(); // convert to string
+            break;
+        case converter.DECTOBIN:
+            resultMsg = "Binary: ";
+            resultMsg += finalResult;
+            break;
+    }
+    document.getElementById("results").style.borderLeftColor = "green";
+    document.getElementById("results").innerHTML = resultMsg;
+}
 
 function clearText() {
     document.getElementById("inputbox").value = ""; // clear the text
